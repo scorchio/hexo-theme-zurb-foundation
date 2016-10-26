a41c7ec1b123a21ee30c9fb13de5338699513c11
Zoltán Adamek <djscorchio@gmail.com>
2016-10-26 22:04:13 +0200

    Adds livereload.

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,68 @@
 var gulp = require('gulp'),
-    $    = require('gulp-load-plugins')(),
+    $ = require('gulp-load-plugins')(),
     debug = require('gulp-debug'),
     clean = require('gulp-clean'),
+    livereload = require('gulp-livereload'),
     responsive = require('gulp-responsive');
 
 var sassPaths = [
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
+    'bower_components/foundation-sites/scss',
+    'bower_components/motion-ui/src'
 ];
 
-gulp.task('foundation-icons', function() {
+gulp.task('foundation-icons', function () {
     return gulp.src(['assets/**/**'])
         .pipe(gulp.dest('source/'));
 });
 
-gulp.task('photoswipe', function() {
-  return gulp.src(['bower_components/photoswipe/dist/**'])
-            .pipe(gulp.dest('source/photoswipe'))
+gulp.task('photoswipe', function () {
+    return gulp.src(['bower_components/photoswipe/dist/**'])
+        .pipe(gulp.dest('source/photoswipe'))
 });
 
-gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('source/css'));
+gulp.task('sass', function () {
+    return gulp.src('scss/app.scss')
+        .pipe($.sass({
+            includePaths: sassPaths,
+            outputStyle: 'compressed' // if css compressed **file size**
+        })
+            .on('error', $.sass.logError))
+        .pipe($.autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 9']
+        }))
+        .pipe(gulp.dest('source/css'));
 });
 
-gulp.task('jsPublic', ['js'], function() {
-   return gulp.src('source/js/app.js').pipe(gulp.dest('../../public/js'));
+gulp.task('jsPublic', ['js'], function () {
+    return gulp.src('source/js/app.js').pipe(gulp.dest('../../public/js')).pipe(livereload());
 });
-gulp.task('sassPublic', ['sass'], function() {
-    return gulp.src('source/css/app.css').pipe(gulp.dest('../../public/css'));
-});
-
-gulp.task('js', function() {
-  return gulp.src(['bower_components/jquery/dist/jquery.js',
-                    'bower_components/what-input/what-input.js',
-                    'bower_components/foundation-sites/dist/foundation.js',
-                    'js/app.js'])
-            .pipe($.concat('app.js'))
-            .pipe($.uglify())
-            .pipe(gulp.dest('source/js/'));
+gulp.task('sassPublic', ['sass'], function () {
+    return gulp.src('source/css/app.css').pipe(gulp.dest('../../public/css')).pipe(livereload());
 });
 
-gulp.task('watch', ['foundation-icons', 'photoswipe', 'sassPublic', 'jsPublic'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sassPublic']);
-  gulp.watch(['js/**/*.js'], ['jsPublic']);
+gulp.task('js', function () {
+    return gulp.src(['bower_components/jquery/dist/jquery.js',
+        'bower_components/what-input/what-input.js',
+        'bower_components/foundation-sites/dist/foundation.js',
+        'js/app.js'])
+        .pipe($.concat('app.js'))
+        .pipe($.uglify())
+        .pipe(gulp.dest('source/js/'));
 });
 
-gulp.task('cleanResponsiveImages', function() {
+gulp.task('watch', ['foundation-icons', 'photoswipe', 'sassPublic', 'jsPublic'], function () {
+    livereload.listen();
+    gulp.watch(['scss/**/*.scss'], ['sassPublic']);
+    gulp.watch(['js/**/*.js'], ['jsPublic']);
+});
+
+gulp.task('cleanResponsiveImages', function () {
     return gulp.src('../../source/_posts/**/*-responsive-*-*.{png,jpg}')
         .pipe(debug({title: 'clean:'}))
         .pipe(clean({force: true}));
 });
 
-gulp.task('responsiveImages', ['cleanResponsiveImages'], function() {
+gulp.task('responsiveImages', ['cleanResponsiveImages'], function () {
     return gulp.src('../../source/_posts/**/*.{png,jpg}')
         .pipe(responsive({
             '**/*.*': [
@@ -75,7 +77,7 @@ gulp.task('responsiveImages', ['cleanResponsiveImages'], function() {
                     rename: {
                         suffix: "-responsive-mini-256"
                     }
-                },
+                }
             ]
         }, {
             errorOnEnlargement: false,
