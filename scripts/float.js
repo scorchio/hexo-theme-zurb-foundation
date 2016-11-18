e9b650142713512c7f8c75a801d494c96b857a77
Zoltán Adamek <djscorchio@gmail.com>
2016-11-18 23:42:44 +0100

    Adds float block tag.

---
+++ scripts/float.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var hexo = hexo || {};
+
+var float = function (args, content) {
+    var direction = args.shift();
+    if (!direction) return;
+    return '<div class="float-' + direction + '">' + hexo.render.renderSync({text: content, engine: 'markdown'}) + '</div>';
+};
+
+hexo.extend.tag.register('float', float, true);
