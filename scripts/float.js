393953b5bd6562d41eeca8431426047a0dd51aba
Zoltán Adamek <djscorchio@gmail.com>
2017-01-02 22:57:41 +0100

    Adds float, figure, header styling fixes.

--- scripts/float.js
+++ scripts/float.js
@@ -5,7 +5,7 @@ var hexo = hexo || {};
 var float = function (args, content) {
     var direction = args.shift();
     if (!direction) return;
-    return '<div class="float-' + direction + '">' + hexo.render.renderSync({text: content, engine: 'markdown'}) + '</div>';
+    return '<div class="float float-' + direction + '">' + hexo.render.renderSync({text: content, engine: 'markdown'}) + '</div>';
 };
 
 hexo.extend.tag.register('float', float, true);
